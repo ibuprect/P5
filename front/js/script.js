611ede70d850669adf62dad9a90bf0ca65ece818
ibuprect <69196500+ibuprect@users.noreply.github.com>
2022-07-22 18:54:30 +0200

    Add files via upload

---
+++ front/js/script.js
@@ -0,0 +1,25 @@
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((objetProduits) => {
+    console.table(objetProduits);
+    lesKanaps(objetProduits);
+  })
+  .catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur ressource api:" + err);
+  });
+ 
+ 
+ 
+function lesKanaps(index) {
+  let zoneArticle = document.querySelector("#items");
+  for (let article of index) {
+    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <h3 class="productName">${article.name}</h3>
+      <p class="productDescription">${article.description}</p>
+    </article>
+  </a>`;
+  }
+}
